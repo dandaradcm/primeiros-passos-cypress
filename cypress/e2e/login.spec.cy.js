803f3a79105deabb3c9afe4c9d776b4b6f81e37c
Guilherme Oliveira <dandaradcm98@gmail.com>
2026-06-10 18:35:16 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,17 +11,17 @@ describe('Orange HRM Tests', () => {
   }
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('userData.userSuccess.username')
-    cy.get(selectorsList.passwordField).type('userData.userSuccess.password')
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
    it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('userData.userFail.username')
-    cy.get(selectorsList.passwordField).type('userData.userFail.password')
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
